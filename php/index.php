c99cd630102c5a6f68e4555c3570a4d9443ac77d
Pedro Gardim <me@pedrogardim.com>
2023-11-09 21:02:02 +0100

    feat: add game cycle logic

--- php/index.php
+++ php/index.php
@@ -12,9 +12,14 @@ class GameOfLife
     {
         $width = intval(exec('tput cols'));
         $height = intval(exec('tput lines'));
-        $this->game_board = array_fill(0, $width, array_fill(0, $height, 0));
+        $this->game_board = array_fill(0, $height, array_fill(0, $width, 0));
         $this->randomize();
-        print_r($this->game_board);
+        $this->draw();
+        while (true) {
+            usleep(200 * 1000);
+            $this->game_cycle();
+            $this->draw();
+        }
     }
 
     public function randomize()
@@ -24,6 +29,38 @@ class GameOfLife
                 $this->game_board[$row][$col] = rand(0, 1);
             }
         }
+    }
+
+    public function game_cycle()
+    {
+        $new_game = $this->game_board;
+        foreach ($this->game_board as $row => $_) {
+            foreach ($this->game_board[$row] as $col => $_) {
+                $neighbours = array(
+                    @$this->game_board[$row + 1][$col - 1],
+                    @$this->game_board[$row + 1][$col],
+                    @$this->game_board[$row + 1][$col + 1],
+                    @$this->game_board[$row - 1][$col - 1],
+                    @$this->game_board[$row - 1][$col],
+                    @$this->game_board[$row - 1][$col + 1],
+                    @$this->game_board[$row][$col - 1],
+                    @$this->game_board[$row][$col + 1],
+                );
+
+                $living_neighbours = count(array_filter($neighbours));
+
+                $should_live = $this->game_board[$row][$col];
+
+                if ($living_neighbours < 2 || $living_neighbours > 3) {
+                    $should_live = false;
+                }
+                if ($living_neighbours == 3) {
+                    $should_live = true;
+                }
+                $new_game[$row][$col] = $should_live;
+            }
+        }
+        $this->game_board = $new_game;
     }
 
     public function draw()
