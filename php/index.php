11751b6ef5a0b25fc5db6a682c5e852318d89256
Pedro Gardim <me@pedrogardim.com>
2023-11-09 19:25:13 +0100

    refactor: randomize method abstracted

--- php/index.php
+++ php/index.php
@@ -13,12 +13,17 @@ class GameOfLife
         $width = intval(exec('tput cols'));
         $height = intval(exec('tput lines'));
         $this->game_board = array_fill(0, $width, array_fill(0, $height, 0));
+        $this->randomize();
+        print_r($this->game_board);
+    }
+
+    public function randomize()
+    {
         foreach ($this->game_board as $row => $_) {
             foreach ($this->game_board as $col => $_) {
                 $this->game_board[$row][$col] = rand(0, 1);
             }
         }
-        print_r($this->game_board);
     }
 }
 
